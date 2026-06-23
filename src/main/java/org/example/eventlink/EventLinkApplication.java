a079491f3f0b07c70d7325808935a70b98458190
김준현 <smileehd03@dgsw.hs.kr>
2026-06-23 11:18:35 +0900

    first commit

---
+++ src/main/java/org/example/eventlink/EventLinkApplication.java
@@ -0,0 +1,13 @@
+package org.example.eventlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventLinkApplication.class, args);
+    }
+
+}
