62b9c8d27df6659044f53252ccd978e333d3a48c
shawnatak926 <poomepoo90@gmail.com>
2026-06-24 09:25:55 +0900

    feat : Participant 엔티티 및 Repository 추가

---
+++ src/main/java/org/example/eventlink/participant/entity/Participant.java
@@ -0,0 +1,34 @@
+package org.example.eventlink.participant.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String studentNumber;
+
+    private Long eventId;
+
+    private LocalDateTime appliedAt;
+}
