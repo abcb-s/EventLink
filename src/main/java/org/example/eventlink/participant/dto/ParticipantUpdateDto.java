17d64de8a193668f41cddd7608715f44736b94c9
shawnatak926 <poomepoo90@gmail.com>
2026-06-24 09:27:06 +0900

    feat : Participant 요청 및 응답 DTO 추가

---
+++ src/main/java/org/example/eventlink/participant/dto/ParticipantUpdateDto.java
@@ -0,0 +1,18 @@
+package org.example.eventlink.participant.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ParticipantUpdateDto {
+
+    @NotBlank(message = "이름을 입력해주세요.")
+    private String name;
+
+    @NotBlank(message = "학번을 입력해주세요.")
+    private String studentNumber;
+}
